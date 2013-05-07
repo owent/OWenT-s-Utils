18fc051f80d2fc9be81e1f2ca643f2b864486af5
OWenT <owt5008137@live.com>
2013-05-07 12:24:29 +0800

    更新其他部分

---
+++ demos/sharpchess/chess.js
@@ -0,0 +1,377 @@
+﻿var myBody;
+var data = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+var dataBtn = [['', '', ''], ['', '', ''], ['', '', '']];
+var AISelecter;
+var OriAIWin = 0;
+var GodAIWin = 0;
+var PlayerWinOri = 0;
+var PlayerWinGod = 0
+var NoneWin = 0;
+var GameOver = false;
+var recorder = document.createElement("div");
+function init() {
+    if (window.addEventListener) {
+        var attach = function (el, ev, handler) {
+            el.addEventListener(ev, handler, false);
+        }
+    } else if (window.attachEvent) {
+        var attach = function (el, ev, handler) {
+            el.attachEvent('on' + ev, handler);
+        }
+    } else {
+        var attach = function (el, ev, handler) {
+            ev['on' + ev] = handler;
+        }
+    }
+
+    var tmpDiv = document.createElement("div");
+    var tmpAuthor = document.createElement("div");
+    tmpAuthor.innerHTML = "2010年1月7日更新<br />●增强浏览器兼容（IE6,IE7）<br />●增加统计功能<br />●最新版已经找不到了，这个纯当留念";
+    var tmpBtnReplay = document.createElement("input");
+    AISelecter = document.createElement("select");
+    var tmpaisopn1 = document.createElement("option");
+    tmpaisopn1.innerHTML = "攻击型AI";
+    var tmpaisopn2 = document.createElement("option");
+    tmpaisopn2.innerHTML = "防御型AI";
+    AISelecter.appendChild(tmpaisopn1);
+    AISelecter.appendChild(tmpaisopn2);
+    tmpBtnReplay.setAttribute("type", "button");
+    tmpBtnReplay.value = "新游戏(玩家先手)";
+    attach(tmpBtnReplay, "click", function () { play(0); });
+    var tmpBtnReplay2 = document.createElement("input");
+    tmpBtnReplay2.setAttribute("type", "button");
+    tmpBtnReplay2.value = "新游戏(电脑先手)";
+    attach(tmpBtnReplay2, "click", function () { play(1); });
+
+    tmpDiv.setAttribute("align", "center");
+    myBody = document.getElementsByTagName("body").item(0);
+    var tmpC1 = document.createElement("h1");
+    tmpC1.innerHTML = "一个简单的#棋智能,你能赢吗?";
+    var tmpC2 = document.createElement("table");
+    for (var i = 0; i < 3; i++) {
+        var tmpRow = tmpC2.insertRow(tmpC2.rows.length);
+        for (var j = 0; j < 3; j++) {
+            var tmpCell = tmpRow.insertCell(tmpRow.cells.length);
+            var tmpClBtn = document.createElement("input");
+            tmpClBtn.value = i + ":" + j;
+            tmpClBtn.setAttribute("type", "button");
+            attach(tmpClBtn, "click", function(ev){
+				for(var tmpi = 0 ; tmpi < 3 ; tmpi ++)
+					for(var tmpj = 0 ; tmpj < 3 ; tmpj ++)
+						if(dataBtn[tmpi][tmpj] == this)
+							OnBtnClick(tmpi, tmpj);
+            });
+            dataBtn[i][j] = tmpClBtn;
+            tmpCell.appendChild(tmpClBtn);
+        }
+    }
+    tmpDiv.appendChild(tmpC1);
+	tmpDiv.appendChild(recorder);
+    tmpDiv.appendChild(tmpBtnReplay);
+    tmpDiv.appendChild(tmpBtnReplay2);
+    tmpDiv.appendChild(AISelecter);
+    tmpDiv.appendChild(tmpC2);
+    myBody.appendChild(tmpDiv);
+	myBody.appendChild(tmpAuthor);
+    show();
+	formatRecord();
+}
+function formatRecord() {
+    recorder.innerHTML = "攻击型AI胜利次数：" 
+		+ GodAIWin + "&nbsp;&nbsp;&nbsp;&nbsp;防御型AI胜利次数："
+		+ OriAIWin + "<br />玩家胜利次数(对手为攻击型AI)："
+		+ PlayerWinGod + "&nbsp;&nbsp;&nbsp;&nbsp;玩家胜利次数(对手为防御型AI)："
+		+ PlayerWinOri + "<br />平局次数："
+		+ NoneWin;
+}
+function reloadD() {
+	GameOver = false;
+    for (var i = 0; i < 3; i++)
+        for (var j = 0; j < 3; j++)
+        data[i][j] = 0;
+    show();
+}
+function pauseGame() {
+    for (var i = 0; i < 3; i++)
+        for (var j = 0; j < 3; j++)
+        dataBtn[i][j].disabled = "disabled";
+}
+function show() {
+    for (var i = 0; i < 3; i++)
+        for (var j = 0; j < 3; j++)
+        if (data[i][j] == 0) {
+        dataBtn[i][j].value = "";
+        dataBtn[i][j].removeAttribute("disabled");
+        dataBtn[i][j].className = "commonChess";
+    }
+    else if (data[i][j] == 1) {
+        dataBtn[i][j].value = "●";
+        dataBtn[i][j].disabled = "disabled";
+        dataBtn[i][j].className = "myChess";
+    }
+    else if (data[i][j] == 2) {
+        dataBtn[i][j].value = "□";
+        dataBtn[i][j].disabled = "disabled";
+        dataBtn[i][j].className = "computerChess";
+    }
+}
+function OnBtnClick(r, c) {
+    if (data[r][c] == 2) {
+        alert("这个位置已经被电脑走掉了,请选择其他位置");
+        return;
+    }
+    else if (data[r][c] == 1) {
+        alert("这个位置已经被你走掉了,请选择其他位置");
+        return;
+    }
+    else if (data[r][c] == 0) {
+        data[r][c] = 1;
+        show();
+        checkSul();
+        computerTurn();
+    }
+}
+function getVectory() {
+    var i, j;
+    var gisover = false;
+    var win = 0;
+    for (i = 0; i < 3; i++) {
+        var isover = true;
+        for (j = 1; j < 3; j++) {
+            if (data[i][j] != data[i][j - 1] || data[i][j] <= 0) {
+                isover = false;
+                if (data[i][j - 1] > 0 && data[i][j] > 0) { win--; break; }
+            }
+        }
+        if (isover) {
+            gisover = isover;
+            win = data[i][0];
+            return win;
+        }
+        isover = true;
+        for (j = 1; j < 3; j++) {
+            if (data[j][i] != data[j - 1][i] || data[j][i] <= 0) {
+                isover = false;
+                if (data[j - 1][i] > 0 && data[j][i] > 0) { win--; break; }
+            }
+        }
+        if (isover) {
+            gisover = isover;
+            win = data[0][i];
+            return win;
+        }
+    }
+
+    if (!gisover) {
+        var isover = true;
+        for (j = 1; j < 3; j++) if (data[j][j] != data[j - 1][j - 1] || data[j][j] <= 0) {
+            isover = false;
+            if (data[j - 1][j - 1] > 0 && data[j][j] > 0) { win--; break; };
+        }
+        if (isover) { gisover = isover; win = data[0][0]; return win; }
+    }
+    if (!gisover) {
+        var isover = true;
+        for (j = 1; j < 3; j++) if (data[j][2 - j] != data[j - 1][3 - j] || data[j][2 - j] <= 0) {
+            isover = false;
+            if (data[j - 1][3 - j] > 0 && data[j][2 - j] > 0) { win--; break; }
+        }
+        if (isover) { gisover = isover; win = data[0][2]; return win; }
+    }
+    return win;
+}
+function checkSul() {
+	if(GameOver)
+		return;
+    var win = getVectory();
+    if (win == 1){
+        alert("恭喜你,你赢了"), pauseGame();
+        if (AISelecter.value == "攻击型AI")
+			PlayerWinGod ++;
+		else
+			PlayerWinOri ++;
+		GameOver = true;
+	}
+    else if (win == 2){
+        alert("很遗憾,你输了\n再来一盘吧"), pauseGame();
+        if (AISelecter.value == "攻击型AI")
+			GodAIWin ++;
+		else
+			OriAIWin ++;
+		GameOver = true;
+	}
+    else if (win <= -8){
+        alert("平局了"), pauseGame();
+		NoneWin ++;
+		GameOver = true;
+	};
+	formatRecord();
+}
+function play(id) { reloadD(); if (id == 1) computerTurn(); }
+
+function testFill(func1, func2) {
+    for (var i = 0; i < 3; ++i) {
+        for (var j = 0; j < 3; ++j) {
+            if (data[i][j] != 0)
+                continue;
+            data[i][j] = 2;
+            if (func2)
+                func2.apply(this, [i, j]);
+            data[i][j] = 1;
+            if (func1)
+                func1.apply(this, [i, j]);
+            data[i][j] = 0;
+        }
+    }
+}
+
+function findDiagonal1() {
+    var c1 = 0, c2 = 0, cp1, cp2;
+    var dia = [8, -4, 6, -4, -4, -4, 2, -4, 0];
+    for (var i = 0; i < 3; ++i)
+        for (var j = 0; j < 3; j++)
+            if (data[i][j] == 1)
+                ++c1, cp1 = i * 3 + j;
+            else if (data[i][j] == 2)
+                ++c2, cp2 = i * 3 + j;
+    
+    return [c1, c2, (c1 == 1) ? [parseInt(dia[cp1] / 3), dia[cp1] % 3] : null, (c2 == 1) ? [parseInt(dia[cp2] / 3), dia[cp2] % 3] : null];
+}
+
+function findWillWin(v) {
+    for (var i = 0; i < 3; ++i) {
+        for (var j = 0; j < 3; j++) {
+            if (data[i][j] != 0)
+                continue;
+            data[i][j] = v;
+            if (getVectory() == v) {
+                data[i][j] = 0;
+                return [i, j];
+            }
+            data[i][j] = 0;
+        }
+    }
+
+    return null;
+}
+
+function checkWillWin(v, t, f) {
+    var r = findWillWin(v);
+    if (r)
+        data[r[0]][r[1]] = t;
+    f.apply(this, []);
+    if (r)
+        data[r[0]][r[1]] = 0;
+}
+
+function computerTurn() {
+    var score = [[2, 0, 2], [0, 1, 0], [2, 0, 2]];
+
+    // 直接胜利、直接堵截
+    testFill(function (x, y) {
+        if (getVectory() == 1)
+            score[x][y] += 32767;
+    }, function (x, y) {
+        if (getVectory() == 2)
+            score[x][y] += 65536;
+    });
+
+    // 一次预测胜利、一次预测堵截
+    testFill(function (x, y) {
+        if (getVectory() == 1)
+            return;
+
+        //checkWillWin(1, 2, function () {
+            testFill(function () {
+                if (getVectory() == 1)
+                    score[x][y] += 1024;
+            }, null);
+        //});
+    }, function (x, y) {
+        if (getVectory() == 2)
+            return;
+
+        //checkWillWin(2, 1, function () {
+            testFill(null, function () {
+                if (getVectory() == 2)
+                    score[x][y] += 4096;
+            });
+        //});
+    });
+
+    // 两次预测胜利、两次预测堵截
+    testFill(function (x, y) {
+        if (getVectory() == 1)
+            return;
+
+        checkWillWin(1, 2, function () {
+            testFill(function () {
+                if (getVectory() == 1)
+                    return;
+
+                checkWillWin(1, 2, function () {
+                    testFill(function () {
+                        if (getVectory() == 1)
+                            score[x][y] += 4;
+                    }, null);
+                });
+            }, null);
+        });
+
+    }, function (x, y) {
+        if (getVectory() == 2)
+            return;
+
+        checkWillWin(2, 1, function () {
+            testFill(null, function () {
+                if (getVectory() == 2)
+                    return;
+
+                checkWillWin(2, 1, function () {
+                    testFill(null, function () {
+                        if (getVectory() == 2)
+                            score[x][y] += 16;
+                    });
+                });
+                
+            });
+        });
+    });
+    
+    // 特殊防御策略
+    var dia_info = findDiagonal1();
+    if (dia_info[2] !== null && dia_info[2][0] >= 0 && dia_info[1] == 0)
+        score[1][1] += 1024;
+    if (dia_info[0] == 2 && dia_info[1] == 1) {
+        if ((data[0][0] == 1 && data[2][2] == 1) ||
+            (data[0][2] == 1 && data[2][0] == 1)) {
+            score[0][1] += 2048;
+            score[2][1] += 2048;
+            score[1][0] += 2048;
+            score[1][2] += 2048;
+        }
+    }
+
+    // 攻击型AI特殊攻击策略
+    if (AISelecter.value == "攻击型AI") {
+        if (dia_info[0] == 1 && dia_info[1] == 1 && data[1][1] == 1)
+            score[dia_info[3][0]][dia_info[3][1]] += 4096 + 2048;
+    }
+
+    // 随机选分数最大的
+    var max_score = 0, max_pos = [];
+    for (var i = 0; i < 3; ++i)
+        for (var j = 0; j < 3; ++j)
+            if (data[i][j] == 0)
+                max_score = Math.max(max_score, score[i][j]);
+
+    for (var i = 0; i < 3; ++i)
+        for (var j = 0; j < 3; ++j)
+            if (max_score == score[i][j] && data[i][j] == 0)
+                max_pos.push([i, j]);
+
+    max_score = parseInt(Math.random() * 10000 * max_pos.length) % max_pos.length;
+    data[max_pos[max_score][0]][max_pos[max_score][1]] = 2;
+
+    show(); checkSul();
+}
---
+++ resource/js/jquery.browser.js
@@ -0,0 +1,463 @@
+window.Util = window.Util || {};
+
+/**
+ * Environment judge
+ * Support browser: IE, Firefox, Chrome, Opera, Safari, Konqueror, Go
+ * jQuery plugin
+ * @version	1.6
+ * @since	1.1
+ * @example	var bro = new Util._TEnvironment();
+ * @example	var bro = Util.getEnvironment();
+ * @example	jQuery.environment;
+ */
+Util._TEnvironment = function () {
+	// default names
+	this._browserName = "Unknown";
+	this._browserVersion = "Unknown";
+	this._browserShort = "Unknown";
+	this._addtional = "";
+	this._OSName = "Unknown";
+	this._kbrow = "Unknown";
+	this._kos = "0.0";
+	this._kver = "Unknown";
+	this._platos = "32-Bit Edition";
+	this._platbr = "32-Bit Edition";
+	this._ismobileos = false;
+	this._ismobilebr = false;
+	this._cookieEnabled;
+	this._platform = navigator.platform || undefined;
+	this._userAgent;
+	this._fakeopera = false;
+	this._compatMode = document.compatMode? document.compatMode: "Unknown";
+	this._isCompatMode = false;
+
+	//---------
+	var _this = this;
+
+	this.toString = function () {
+		var t = _this._browserName;
+		if (_this._browserVersion != "Unknown")
+			t += " " + _this._browserVersion;
+		if (_this._platbr == "64-Bit Edition")
+			t += " " + _this._platbr;
+		if (_this._addtional != "")
+			t += _this._addtional;
+		if (_this._OSName != "Unknown")
+			t += " on " + _this._OSName;
+		if (_this._platos == "64-Bit Edition" && _this._kos != "Apple Darwin")
+			t += " " + _this._platos;
+		return t;
+	};
+
+	this.capture = function () {
+		var ua = navigator.userAgent.toLowerCase();
+		if (_this._kbrow == "Microsoft Trident by IE") {
+			if (ua.match(/.net clr 2.0/))
+				_this._dotnet2_0 = true;
+			if (ua.match(/.net clr 3.0/))
+				_this._dotnet3_0 = true;
+			if (ua.match(/.net clr 3.5/))
+				_this._dotnet3_5 = true;
+			if (ua.match(/.net4.0c/)) {
+				if (ua.match(/.net4.0e/))
+					_this._dotnet4_0full = true;
+				_this._dotnet4_0client = true;
+			}
+			try {
+				var t = new ActiveXObject("AgControl.AgControl");
+				_this._silverlight = true;
+			} catch (e) {
+			}
+		} else {
+			if (navigator.plugins["Silverlight Plug-In"]) {
+				_this._silverlight = true;
+			}
+		}
+		if (_this._silverlight) {
+			if (_this.canRunSilverlight("5.0"))
+				_this._silverlight5_0 = true;
+			if (_this.canRunSilverlight("4.0"))
+				_this._silverlight4_0 = true;
+			if (_this.canRunSilverlight("3.0"))
+				_this._silverlight3_0 = true;
+			if (_this.canRunSilverlight("2.0"))
+				_this._silverlight2_0 = true;
+		}
+	};
+
+	this.canRunSilverlight = function (d) {
+		if (!_this._silverlight)
+			return false;
+		var c = false, a = null;
+		try {
+			var b = null;
+			var v = window.navigator.userAgent;
+			if (_this._kbrow == "Microsoft Trident by IE") {
+				b = new ActiveXObject("AgControl.AgControl");
+			}
+			else {
+				a = document.createElement("div");
+				document.body.appendChild(a);
+				if (_this._kbrow == "Apple WebKit(KHTML based)")
+					a.innerHTML = '<embed type="application/x-silverlight" />';
+				else
+					a.innerHTML = '<object type="application/x-silverlight"  data="data:," />';
+				b = a.childNodes[0];
+			}
+			if (b.IsVersionSupported(d))
+				c = true;
+			b = null;
+		} catch (e) {
+			c = false;
+		}
+		try {
+			if (a)
+				document.body.removeChild(a);
+		} catch (e) {
+			a.innerHTML = "";
+		}
+		return c;
+	}
+
+
+	//init
+	var init = (function () {
+		//Copy
+		_this._userAgent = navigator.userAgent;
+		_this._cookieEnabled = navigator.cookieEnabled;
+		var ua = _this._userAgent.toLowerCase();
+
+		//OS
+		if (ua.match(/windows/)) {
+			if (ua.match(/windows ce/)) {
+				_this._OSName = "Microsoft Windows CE/Windows Mobile";
+				_this._ismobileos = true;
+			} else if (ua.match(/windows 95/)) {
+				_this._OSName = "Microsoft Windows 95";
+			} else if (ua.match(/windows 98/)) {
+				if (ua.match(/win 9x 4.90/))
+					_this._OSName = "Microsoft Windows ME";
+				else
+					_this._OSName = "Microsoft Windows 98";
+			} else if (ua.match(/windows phone os/)) {
+				try {
+					var t_ver = ua.match(/windows phone os ([\d.]+)/)[1];
+					_this._OSName = "Microsoft Windows Phone " + t_ver;
+				} catch (e) {
+					_this._OSName = "Microsoft Windows Phone";
+				}
+				_this._ismobileos = true;
+			} else if (ua.match(/windows nt/)) {
+				try {
+					var t_ver = ua.match(/windows nt ([\d.]+)/)[1];
+					var is64 = false;
+					if (ua.match(/win64|wow64|x64|ia64/)) {
+						is64 = true;
+						_this._platos = "64-Bit Edition";
+					}
+					if (t_ver == "6.2") {
+						_this._OSName = "Microsoft Windows 8";
+					} else if (t_ver == "6.1") {
+						_this._OSName = "Microsoft Windows 7/Windows Server 2008 R2";
+					} else if (t_ver == "6.0") {
+						_this._OSName = "Microsoft Windows Vista/Windows Server 2008";
+					} else if (t_ver == "5.2") {
+						if (is64)
+							_this._OSName = "Microsoft Windows XP/Windows Server 2003";
+						else
+							_this._OSName = "Microsoft Windows Server 2003";
+					} else if (t_ver == "5.1") {
+						_this._OSName = "Microsoft Windows XP";
+					} else if (t_ver == "5.0") {
+						_this._OSName = "Microsoft Windows 2000";
+					} else {
+						_this._OSName = "Microsoft Windows NT";
+					}
+				} catch (e) {
+					_this._OSName = "Microsoft Windows NT";
+				}
+			} else
+				_this._OSName = "Microsoft Windows";
+			_this._kos = "Microsoft Windows";
+		} else if (ua.match(/linux/)) {
+			if (ua.match(/android/)) {
+				try {
+					var t_ver = ua.match(/android ([\d.]+)/)[1];
+					_this._OSName = "Google Android " + t_ver;
+				} catch (e) {
+					_this._OSName = "Google Android";
+				}
+				_this._ismobileos = true;
+			} else if (ua.match(/ubuntu|fedora|redhat|debian|suse/)) {
+				var strs = ['Ubuntu', 'Fedora', 'RedHat', 'Debian', 'SUSE'];
+				var tmp = "Unknown";
+				for (var i = 0; i < strs.length; i++) {
+					if (ua.match(strs[i].toLowerCase())) {
+						tmp = strs[i];
+						break;
+					}
+				}
+				tmp += " Linux";
+				_this._OSName = tmp;
+			}
+			else
+				_this._OSName = "Linux";
+			if (ua.match(/x86_64/))
+				_this._platos = "64-Bit Edition";
+			_this._kos = "OpenSource Linux";
+		} else if (ua.match(/macintosh|iphone|ipad|ipod/)) {
+			if (ua.match(/macintosh/)) {
+				_this._OSName = "Apple Mac OS X";
+			} else {
+				_this._OSName = "Apple iOS";
+				_this._ismobileos = true;
+			}
+			try {
+				var t_ver = ua.match(/(ppc mac os x|intel mac os x|cpu iphone os|cpu os) ([\d_]+)/)[2];
+				t_ver = t_ver.replace(/_/g, ".");
+				_this._OSName += " " + t_ver;
+			} catch (e) {
+			}
+			if (_this._OSName.match(/Apple Mac OS X 10.(6|7)/)) {
+				_this._platos = "64-Bit Edition";
+			}
+			_this._kos = "Apple Darwin";
+		} else if (ua.match(/blackberry/)) {
+			_this._OSName = "BlackBerry OS";
+			_this._kos = "RIM";
+			_this._ismobileos = true;
+		} else if (ua.match(/symbianos|symbos/)) {
+			_this._OSName = "Nokia Symbian";
+			_this._kos = "Nokia Symbian";
+			_this._ismobileos = true;
+		} else if (ua.match(/solaris|sunos/)) {
+			_this._OSName = "Sun Solaris";
+			_this._kos = "OpenSource Unix-Like";
+		} else if (ua.match(/bsd/)) {
+			if (ua.match(/freebsd|openbsd/))
+				_this._OSName = _this._userAgent.match(/freebsd|openbsd/i)[1] + " OS";
+			else
+				_this._OSName = "BSD";
+			_this._kos = "OpenSource Unix-Like";
+		} else if (ua.match(/j2me/)) {
+			_this._OSName = "J2ME OS- java based environment";
+			_this._ismobileos = true;
+		}
+
+		//Browser
+		if (_this._ismobileos)
+			_this._ismobilebr = true;
+
+		//Opera
+		if (ua.match(/opera|pesto/)) {
+			_this._browserShort = "Opera";
+			_this._browserName = "Opera ";
+			//Opera Mobile
+			if (ua.indexOf("opera mobi") != -1) {
+				_this._browserName = "Opera Mobile ";
+				_this._ismobilebr = true;
+			}
+			//Opera Mini
+			if (ua.indexOf("opera mini") != -1) {
+				_this._browserName = "Opera Mini ";
+				_this._ismobilebr = true;
+			}
+			if (ua.match(/mozilla/)) {
+				_this._browserVersion = ua.match(/opera ([\w.]+)/)[1];
+				_this._fakeopera = true;
+			} else {
+				_this._browserVersion = ua.match(/version\/([\w.]+)/)[1];
+				_this._kver = ua.match(/presto\/([\w.]+)/)[1];
+			}
+			_this._kbrow = "OpenSource Presto";
+		}
+
+		//Trident(Microsoft Internet Explorer Based)
+		else if (ua.match(/msie|trident/)) {
+
+			//The World
+			if (ua.match(/theworld/)) {
+				_this._browserShort = "TheWorld";
+				_this._browserName = "TheWorld Browser";
+				_this._browserVersion = "";
+				_this._addtional = " with Microsoft Internet Explorer " + ua.match(/msie ([\w.]+)/)[1] + " kernel";
+			}
+			//Tencent Traveler
+			else if (ua.match(/tencenttraveler/)) {
+				_this._browserShort = "Traveler";
+				_this._browserName = "Tencent Traveler ";
+				_this._browserVersion = ua.match(/tencenttraveler ([\w.]+)/)[1];
+				_this._addtional = " with Microsoft Internet Explorer " + ua.match(/msie ([\w.]+)/)[1] + " kernel";
+			}
+			//Maxthon
+			else if (ua.match(/maxthon/)) {
+				_this._browserShort = "Maxthon";
+				_this._browserName = "Maxthon ";
+				if (ua.match(/maxthon(\/| )([\w.]+)/)) {
+					_this._browserVersion = ua.match(/maxthon(\/| )([\w.]+)/)[1];
+				} else {
+					_this._browserVersion = "1.0";
+				}
+				_this._addtional = " with Microsoft Internet Explorer " + ua.match(/msie ([\w.]+)/)[1] + " kernel";
+			}
+			//Avant
+			else if (ua.match(/avant/)) {
+				_this._browserShort = "avant";
+				_this._browserName = "avant Browser ";
+				_this._addtional = " with Microsoft Internet Explorer " + ua.match(/msie ([\w.]+)/)[1] + " kernel";
+			}
+			//360SE
+			else if (ua.match(/360se/)) {
+				_this._browserShort = "360SE";
+				_this._browserName = "Qihoo 360 Safe Browser ";
+				_this._addtional = " with Microsoft Internet Explorer " + ua.match(/msie ([\w.]+)/)[1] + " kernel";
+			}
+			//Sogou
+			else if (ua.match(/se ([\d.]+)/)) {
+				_this._browserShort = "Sogou";
+				_this._browserName = "Sohu Sogou ";
+				_this._browserVersion = ua.match(/se ([\w.]+)/)[1];
+				_this._addtional = " with Microsoft Internet Explorer " + ua.match(/msie ([\w.]+)/)[1] + " kernel";
+			}
+
+			//Microsoft Internet Explorer && Internet Explorer Mobile
+			else {
+				_this._browserShort = "IE";
+				_this._browserName = "Microsoft Internet Explorer ";
+				_this._browserVersion = ua.match(/msie ([\w.]+)/)[1];
+				if (_this._platos == "64-Bit Edition") {
+					if (ua.match(/win64|x64|ia64/)) {
+						_this._platbr = "64-Bit Edition"
+					}
+				}
+				if (ua.match(/iemobile/)) {
+					_this._ismobilebr = true;
+				}
+			}
+
+			_this._kbrow = "Microsoft Trident by IE";
+			_this._kver = ua.match(/msie ([\w.]+)/)[1];
+			if (parseInt(_this._kver) == 7 && ua.match(/trident/))
+				_this._isCompatMode = true;
+		}
+
+		//KHTML(KDE Konqueror)
+		else if (ua.match(/konqueror|khtml|safari|applewebkit/)) {
+			//Webkit(Apple Safari)
+			if (ua.match(/safari|applewebkit/)) {
+				//Tencent Traveler
+				if (ua.match(/tencenttraveler/)) {
+					_this._browserShort = "Traveler";
+					_this._browserName = "Tencent Traveler ";
+					_this._browserVersion = ua.match(/tencenttraveler ([\w.]+)/)[1];
+					_this._addtional = " with Apple Safari kernel";
+				}
+				//Maxthon
+				else if (ua.match(/maxthon/)) {
+					_this._browserShort = "Maxthon";
+					_this._browserName = "Maxthon ";
+					if (ua.match(/maxthon(\/| )([\w.]+)/)) {
+						_this._browserVersion = ua.match(/maxthon(\/| )([\w.]+)/)[1];
+					} else {
+						_this._browserVersion = "1.0";
+					}
+					_this._addtional = " with Apple Safari kernel";
+				}
+				//360SE
+				else if (ua.match(/360se/)) {
+					_this._browserShort = "360SE";
+					_this._browserName = "Qihoo 360 Safe Browser ";
+					_this._addtional = " with Apple Safari kernel";
+				}
+				//Sogou
+				else if (ua.match(/se ([\d.]+)/)) {
+					_this._browserShort = "Sogou";
+					_this._browserName = "Sohu Sogou ";
+					_this._browserVersion = ua.match(/se ([\w.]+)/)[1];
+					_this._addtional = " with Apple Safari kernel";
+				}
+				//BrowserNG
+				else if (ua.match(/browserng/)) {
+					_this._browserShort = "BrowNG";
+					_this._browserName = "Nokia BrowserNG ";
+					_this._browserVersion = ua.match(/browserng\/([\w.]+)/)[1];
+					_this._addtional = " with Apple Safari kernel";
+					_this._ismobilebr = true;
+				}
+				//Google Chrome && Chrome Lite
+				else if (ua.match(/chrome|chromium|android/)) {
+					//Chrome Plus
+					if (ua.match(/chromeplus/)) {
+						_this._browserShort = "Chrome Plus";
+						_this._browserName = "Chrome Plus ";
+						_this._browserVersion = ua.match(/chromeplus\/([\w.]+)/)[1];
+						_this._addtional = " with Google Chrome kernel";
+					} else {
+						//Google Chrome && Chrome Lite
+						_this._browserShort = "Chrome";
+						_this._browserName = "Google Chrome ";
+						if (ua.match(/android/) && ua.match(/version\/([\w.]+)/)) {
+							_this._browserName = "Google Chrome Lite ";
+							_this._browserVersion = ua.match(/version\/([\w.]+)/)[1];
+							_this._ismobilebr = true;
+						} else if (ua.match(/chrome/)) {
+							_this._browserVersion = ua.match(/chrome\/([\w.]+)/)[1];
+						} else {
+							_this._browserVersion = ua.match(/chromium\/([\w.]+)/)[1];
+						}
+					}
+				}
+
+				//Apple Safari
+				else {
+					_this._browserShort = "Safari";
+					_this._browserName = "Apple Safari ";
+					_this._browserVersion = ua.match(/version\/([\w.]+)/)[1];
+					if (_this._platos == "64-Bit Edition") {
+						_this._platbr = "64-Bit Edition"
+					}
+				}
+				_this._kbrow = "Apple WebKit(KHTML based)";
+				_this._kver = ua.match(/applewebkit\/([\w.]+)/)[1];
+			} else {
+				//Konqueror
+				_this._browserShort = "Konqueror";
+				_this._browserName = "KDE Konqueror ";
+				_this._browserVersion = ua.match(/konqueror\/([\w.]+)/)[1];
+				_this._kbrow = "OpenSource KHTML";
+				_this._kver = ua.match(/khtml\/([\w.]+)/)[1];
+			}
+		}
+
+		//Mozilla FireFox
+		else if (ua.match(/firefox|gecko/)) {
+			_this._browserShort = "FF";
+			_this._browserName = "Mozilla FireFox ";
+			_this._browserVersion = ua.match(/firefox\/([\w.]+)/)[1];
+			_this._kbrow = "OpenSource Gecko";
+			_this._kver = ua.match(/gecko\/([\w.]+)/)[1];
+		}
+
+		//Go Browser
+		else if (ua.match(/gobrowser/)) {
+			_this._browserShort = "GoBrow";
+			_this._browserName = "Nokia Go Browser ";
+			_this._browserVersion = ua.match(/gobrowser\/([\w.]+)/)[1];
+			_this._ismobilebr = true;
+		}
+
+		_this.capture();
+	})();
+};
+
+/**
+ * Get the information of current environment.
+ * @package Util
+ * @return The information of current environment.
+ * @example	var bro = Util.getEnvironment();
+ * @version	1.4
+ * @since	1.1
+ */
+Util.getEnvironment = function(){ return new Util._TEnvironment(); };
+
+try{ if(jQuery) jQuery.extend({ environment: Util.getEnvironment() }); } catch (e) {}
