0fc7937d52ce1e4a2b4177c910e80b59b052680b
OWenT <owt5008137@live.com>
2013-07-23 22:09:51 +0800

    Update IncludeDirectoryRecurse.cmake

--- Other/CMake/Modules/IncludeDirectoryRecurse.cmake
+++ Other/CMake/Modules/IncludeDirectoryRecurse.cmake
@@ -63,3 +63,25 @@ macro(include_project_recurse)
 	endforeach()
 endmacro(include_project_recurse)
 
+
+# cmake VC 源文件分组  
+# source_group_by_dir [Source List Var]
+macro(source_group_by_dir)
+    if(MSVC)
+        foreach(source_files ${ARGV})
+            set(sgbd_cur_dir ${CMAKE_CURRENT_SOURCE_DIR})
+            foreach(sgbd_file ${${source_files}})
+                string(REGEX REPLACE ${sgbd_cur_dir}/\(.*\) \\1 sgbd_fpath ${sgbd_file})
+                string(REGEX REPLACE "\(.*\)/.*" \\1 sgbd_group_name ${sgbd_fpath})
+                string(COMPARE EQUAL ${sgbd_fpath} ${sgbd_group_name} sgbd_nogroup)
+                string(REPLACE "/" "\\" sgbd_group_name ${sgbd_group_name})
+                if(sgbd_nogroup)
+                    set(sgbd_group_name "\\")
+                endif(sgbd_nogroup)
+                source_group(${sgbd_group_name} FILES ${sgbd_file})
+            endforeach(sgbd_file)
+        endforeach()
+    endif(MSVC)
+endmacro(source_group_by_dir)
+
+
