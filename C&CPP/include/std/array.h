0ae98a1e13cfe2903520d4c744ac57683997fe2b
OWenT <owt5008137@live.com>
2013-10-28 10:24:50 +0800

    C++ 0x/11 编译器检测优化

--- C&CPP/include/std/array.h
+++ C&CPP/include/std/array.h
@@ -17,12 +17,12 @@
 #if defined(_MSC_VER) && (_MSC_VER >= 1020)
 # pragma once
 #endif
- 
+
 // ============================================================
 // 公共包含部分
 // 自动导入TR1库
 // ============================================================
-  
+ 
 /**
 * 导入数组（array）
 * 如果是G++且支持c++0x草案1（tr1版本）的array[GCC版本高于4.0]
@@ -33,14 +33,16 @@
 *
 * 否则启用boost中的array库（如果是这种情况需要加入boost库）
 */
-  
+ 
 // VC9.0 SP1以上分支判断
-#if defined(_MSC_VER) && (_MSC_VER == 1500 && defined (_HAS_TR1) || _MSC_VER > 1500)
+#if defined(_MSC_VER) && (_MSC_VER == 1500 && defined (_HAS_TR1)) || (_MSC_VER > 1500 && defined(_HAS_CPP0X) && _HAS_CPP0X)
     // 采用VC std::tr1库
     #include <array>
 #elif defined(__GNUC__) && __GNUC__ >= 4
     // 采用G++ std::tr1库
-    #if !defined(__GXX_EXPERIMENTAL_CXX0X__) && !defined(__clang__)
+    #if __cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__)
+        #include <array>
+    #else
         #include <tr1/array>
         namespace std {
             using tr1::array;
@@ -48,10 +50,8 @@
             using tr1::tuple_element;
             using tr1::tuple_size;
         }
-    #else
-        #include <array>
     #endif
- 
+
 #else
     // 采用boost库
     #include <boost/tr1/array.hpp>
@@ -62,5 +62,5 @@
         using tr1::tuple_size;
     }
 #endif
- 
+
 #endif
