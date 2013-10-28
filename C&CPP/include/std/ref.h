5a7293a939a426613291c3518c9b2fc097032382
OWenT <owt5008137@live.com>
2013-10-28 10:27:30 +0800

    C++ 0x/11 编译器检测优化

--- C&CPP/include/std/ref.h
+++ C&CPP/include/std/ref.h
@@ -11,53 +11,53 @@
  *
  */
  
-#ifndef _STD_FUNCTIONAL_H_
-#define _STD_FUNCTIONAL_H_
+#ifndef _STD_REF_H_
+#define _STD_REF_H_
  
 #if defined(_MSC_VER) && (_MSC_VER >= 1020)
 # pragma once
 #endif
- 
+
 // ============================================================
 // 公共包含部分
-// 自动导入TR1库
+// 自动导入引用包装库
 // ============================================================
-  
+ 
 /**
-* 导入引用包装（ref, ref_wrapper）
-* 如果是G++且支持c++0x草案1（tr1版本）的functional[GCC版本高于4.0]
-* 则会启用GNU-C++的functional 中的ref
+* 导入引用包装（std::ref, std::cref, std::reference_wrapper）
+* 如果是G++且支持c++0x草案1（tr1版本）的smart_ptr[GCC版本高于4.0]
+* 则会启用GNU-C++的引用包装
 *
-* 如果是VC++且支持c++0x草案1（tr1版本）的functional[VC++版本高于9.0 SP1]
-* 则会启用VC++的functional
+* 如果是VC++且支持c++0x草案1（tr1版本）的smart_ptr[VC++版本高于9.0 SP1]
+* 则会启用VC++的引用包装
 *
-* 否则启用boost中的functional库（如果是这种情况需要加入boost库）
+* 否则启用boost中的引用包装库（如果是这种情况需要加入boost库）
 */
-  
+ 
 // VC9.0 SP1以上分支判断
-#if defined(_MSC_VER) && (_MSC_VER == 1500 && defined (_HAS_TR1) || _MSC_VER > 1500)
+#if defined(_MSC_VER) && (_MSC_VER == 1500 && defined (_HAS_TR1)) || (_MSC_VER > 1500 && defined(_HAS_CPP0X) && _HAS_CPP0X)
     // 采用VC std::tr1库
     #include <functional>
 #elif defined(__GNUC__) && __GNUC__ >= 4
     // 采用G++ std::tr1库
-    #if !defined(__GXX_EXPERIMENTAL_CXX0X__) && !defined(__clang__)
+    #if __cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__)
+        #include <functional>
+    #else
         #include <tr1/functional>
         namespace std {
             using tr1::ref;
             using tr1::cref;
             using tr1::reference_wrapper;
         }
-    #else
-        #include <functional>
     #endif
 #else
     // 采用boost tr1库
-    #include <boost/tr1/functional>
+    #include <boost/tr1/functional.hpp>
     namespace std {
         using tr1::ref;
         using tr1::cref;
         using tr1::reference_wrapper;
     }
 #endif
- 
+
 #endif
