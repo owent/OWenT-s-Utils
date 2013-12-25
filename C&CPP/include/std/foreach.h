1638480049d19103e1a844ce3f183297861a17a6
OWenT <owt5008137@live.com>
2013-12-25 10:39:18 +0800

    add clang support

--- C&CPP/include/std/foreach.h
+++ C&CPP/include/std/foreach.h
@@ -76,6 +76,9 @@
         // 采用 VC 的 for each (object var in collection_to_loop)
         #define owent_foreach(VAR, COL) for each (VAR in COL)
     #endif
+#elif defined(__clang__) && __clang_major__ >= 3 && defined(__cplusplus) && __cplusplus >= 201103L
+    // 采用 clang 的range-based-for循环
+    #define owent_foreach(VAR, COL) for(VAR : COL)
 #elif defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)) && (__cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__))
     // 采用G++ 的range-based-for循环
     #define owent_foreach(VAR, COL) for(VAR : COL)
