0596aa9df0f9fd193455ca9045a3213b2618108b
OWenT <owt5008137@live.com>
2013-12-25 11:11:31 +0800

    add clang support

--- C&CPP/include/std/functional.h
+++ C&CPP/include/std/functional.h
@@ -38,8 +38,11 @@
 #if defined(_MSC_VER) && (_MSC_VER == 1500 && defined (_HAS_TR1)) || (_MSC_VER > 1500 && defined(_HAS_CPP0X) && _HAS_CPP0X)
     // 采用VC std::tr1库
     #include <functional>
+#elif defined(__clang__) && __clang_major__ >= 3 && (__cplusplus >= 201103L || !defined(_LIBCPP_HAS_NO_VARIADICS))
+    // 采用Clang c++11库
+    #include <functional>
 #elif defined(__GNUC__) && __GNUC__ >= 4 && __GNUC_MINOR__ >= 3
-    // 采用G++ std::tr1库
+    // 采用GCC c++11库
     #if __cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__)
         #include <functional>
     #else
@@ -69,12 +72,12 @@
     // 采用boost tr1库
     #include <boost/tr1/functional>
     namespace std {
-		using tr1::ref;
-		using tr1::cref;
-		using tr1::hash;
-		using tr1::reference_wrapper;
-		using tr1::result_of;
-		using tr1::swap;
+        using tr1::ref;
+        using tr1::cref;
+        using tr1::hash;
+        using tr1::reference_wrapper;
+        using tr1::result_of;
+        using tr1::swap;
 		
         using tr1::bad_function_call;
         using tr1::bind;
