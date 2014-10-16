121f512836acb5301250ed1dbda50eab2be32fe9
OWenT <owt5008137@live.com>
2014-10-16 10:11:50 +0800

    Update RandomTest.cpp

--- C&CPP/test/src/RandomTest.cpp
+++ C&CPP/test/src/RandomTest.cpp
@@ -8,8 +8,8 @@ TEST(RandomTest, RandomGenMT19937)
     util::random::MT19937 stGen1;
     stGen1.InitSeed(123);
 
-    uint32_t a1 = stGen1.Radom();
-    uint32_t a2 = stGen1.Radom();
+    uint32_t a1 = stGen1.Random();
+    uint32_t a2 = stGen1.Random();
     uint32_t a3 = stGen1();
 
 	ASSERT_EQ(static_cast<uint32_t>(2991312382), a1);
@@ -22,8 +22,8 @@ TEST(RandomTest, RandomGenMT19937_64)
     util::random::MT19937_64 stGen1;
     stGen1.InitSeed(321);
 
-    uint64_t a1 = stGen1.Radom();
-    uint64_t a2 = stGen1.Radom();
+    uint64_t a1 = stGen1.Random();
+    uint64_t a2 = stGen1.Random();
     uint64_t a3 = stGen1();
 
     ASSERT_EQ(10254371118423419891ULL, a1);
@@ -36,8 +36,8 @@ TEST(RandomTest, RandomGen64MT11213B)
     util::random::MT11213B stGen1;
     stGen1.InitSeed(789);
 
-    uint64_t a1 = stGen1.Radom();
-    uint64_t a2 = stGen1.Radom();
+    uint64_t a1 = stGen1.Random();
+    uint64_t a2 = stGen1.Random();
     uint64_t a3 = stGen1();
 
     ASSERT_EQ(static_cast<uint32_t>(3740219552), a1);
@@ -50,8 +50,8 @@ TEST(RandomTest, RandomGenTAUS88)
     util::random::TAUS88 stGen1;
     stGen1.InitSeed(321);
 
-    uint32_t a1 = stGen1.Radom();
-    uint32_t a2 = stGen1.Radom();
+    uint32_t a1 = stGen1.Random();
+    uint32_t a2 = stGen1.Random();
     uint32_t a3 = stGen1();
 
     ASSERT_EQ(static_cast<uint32_t>(43258884), a1);
